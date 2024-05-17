370f17d9444b0447506d0c76477480da4698d8be
dommarajunikhil26 <92212838+dommarajunikhil26@users.noreply.github.com>
2024-05-17 12:00:25 -0500

    Initial commit

---
+++ src/test/java/com/nikhil/bookVerse/BookVerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhil.bookVerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookVerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
