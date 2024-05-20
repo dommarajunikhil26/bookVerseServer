ab449651a222f8f6e102a37a4e59f09e9ef91986
dommarajunikhil26 <92212838+dommarajunikhil26@users.noreply.github.com>
2024-05-20 04:35:24 -0500

    Added Search by Title

--- src/main/java/com/nikhil/bookVerse/repository/BookRepository.java
+++ src/main/java/com/nikhil/bookVerse/repository/BookRepository.java
@@ -1,9 +1,13 @@
 package com.nikhil.bookVerse.repository;
 
 import com.nikhil.bookVerse.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Repository
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
