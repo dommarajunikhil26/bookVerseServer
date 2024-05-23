977c5c1609952042a56486e932192a82b717fdc0
dommarajunikhil26 <92212838+dommarajunikhil26@users.noreply.github.com>
2024-05-22 22:53:38 -0500

    Added Checkout entity

--- src/main/java/com/nikhil/bookVerse/controller/BookController.java
+++ src/main/java/com/nikhil/bookVerse/controller/BookController.java
@@ -1,16 +1,40 @@
 package com.nikhil.bookVerse.controller;
 
+import com.nikhil.bookVerse.entity.Book;
 import com.nikhil.bookVerse.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost:5173")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/books")
 public class BookController {
 
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception{
+        String userEmail = "nikhilsai2k@gmail.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+
+    @GetMapping("/secure/ischeckedout/byuser")
+    public Boolean checkoutBookByUser(@RequestParam Long bookId){
+        String userEmail = "nikhilsai2k@gmail.com";
+        return bookService.checkoutBookByUser(userEmail, bookId);
+    }
+
+    @GetMapping("/secure/currentloans/count")
+    public int currentLoansCount(){
+        String userEmail = "nikhilsai2k@gmail.com";
+        return bookService.currentLoansCount(userEmail);
+    }
 }
--- src/main/java/com/nikhil/bookVerse/service/BookService.java
+++ src/main/java/com/nikhil/bookVerse/service/BookService.java
@@ -1,13 +1,59 @@
 package com.nikhil.bookVerse.service;
 
+import com.nikhil.bookVerse.entity.Book;
+import com.nikhil.bookVerse.entity.Checkout;
 import com.nikhil.bookVerse.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import com.nikhil.bookVerse.repository.CheckoutRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.time.LocalDate;
+
+import java.util.Optional;
 
 @Service
+@Transactional
 public class BookService {
+    private BookRepository bookRepository;
+    private CheckoutRepository checkoutRepository;
 
+    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository){
+        this.bookRepository = bookRepository;
+        this.checkoutRepository = checkoutRepository;
+    }
+
+    public Book checkoutBook(String userEmail, Long bookId) throws Exception{
+        Optional<Book> book = bookRepository.findById(bookId);
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if(!book.isPresent() || validateCheckout != null || book.get().getCopiesAvailable() <= 0){
+            throw new Exception("Book doesn't exist or already checked out by user");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() - 1);
+        bookRepository.save(book.get());
+
+        Checkout checkout = new Checkout(
+                userEmail,
+                LocalDate.now().toString(),
+                LocalDate.now().plusDays(7).toString(),
+                book.get().getId()
+        );
+        checkoutRepository.save(checkout);
+
+        return book.get();
+    }
+
+    public Boolean checkoutBookByUser(String userEmail, Long bookId){
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+        if(validateCheckout != null){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int currentLoansCount(String userEmail){
+        return checkoutRepository.findBooksByUserEmail(userEmail).size();
+    }
 }
